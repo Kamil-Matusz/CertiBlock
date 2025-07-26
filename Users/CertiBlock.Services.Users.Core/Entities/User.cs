4b07c578a811f790b581f9382bea2a0735742cac
Awahir <matuszkamil076@gmail.com>
2025-07-26 22:00:18 +0200

    User entites

---
+++ Users/CertiBlock.Services.Users.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using CertiBlock.Services.Users.Core.ValueObjects;
+
+namespace CertiBlock.Services.Users.Core.Entities;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ Users/CertiBlock.Services.Users.Core/Exceptions/CustomException.cs
@@ -0,0 +1,8 @@
+﻿namespace CertiBlock.Services.Users.Core.Exceptions;
+
+public abstract class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
