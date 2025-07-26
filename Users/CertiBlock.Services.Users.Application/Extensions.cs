f33ca74c8b00bc2af4f7c002c0e10c12ee72068f
Awahir <matuszkamil076@gmail.com>
2025-07-26 21:57:06 +0200

    Users-Service init commit

---
+++ Users/CertiBlock.Services.Users.Application/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CertiBlock.Services.Users.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
---
+++ Users/CertiBlock.Services.Users.Infrastructure/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CertiBlock.Services.Users.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseRouting();
+        return app;
+    }
+    
+    public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
+    {
+        var options = new T();
+        var section = configuration.GetRequiredSection(sectionName);
+        section.Bind(options);
+
+        return options;
+    }
+}
