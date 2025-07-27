09a173d39285c3a8798845cc7484a062961ea279
Awahir <matuszkamil076@gmail.com>
2025-07-27 15:31:24 +0200

    Create database connection to the PostgreSQL

---
+++ Users/CertiBlock.Services.Users.Infrastructure/DAL/PostgreSQL/DatabaseInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CertiBlock.Services.Users.Infrastructure.DAL.PostgreSQL;
+
+internal sealed class DatabaseInitializer : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public DatabaseInitializer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        // auto migrations
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+            await dbContext.Database.MigrateAsync(cancellationToken);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
