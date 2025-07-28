1df654bf792b35088dd12c2d3936426ecd3938b5
Awahir <matuszkamil076@gmail.com>
2025-07-28 17:19:29 +0200

    Add users controller

---
+++ Users/CertiBlock.Services.Users.Api/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CertiBlock.Services.Users.Api.Controllers;
+
+[ApiController]
+[Route(BasePath + "/[controller]")]
+public class BaseController : ControllerBase
+{
+    protected const string BasePath = "users-service";
+}
---
+++ Users/CertiBlock.Services.Users.Api/Controllers/UsersController.cs
@@ -0,0 +1,130 @@
+﻿using CertiBlock.Services.Users.Application.Abstractions;
+using CertiBlock.Services.Users.Application.Commands;
+using CertiBlock.Services.Users.Application.Queries;
+using CertiBlock.Services.Users.Application.Security;
+using CertiBlock.Services.Users.Core.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CertiBlock.Services.Users.Api.Controllers;
+
+public class UsersController : BaseController
+{
+    private readonly ICommandHandler<SignUp> _signUpHandler;
+    private readonly ICommandHandler<SignIn> _signInHandler;
+    private readonly ICommandHandler<DeleteUserAccount> _deleteAccountHandler;
+    private readonly ICommandHandler<ChangeUserRole> _changeUserRoleHandler;
+    private readonly IQueryHandler<GetAccountInfo, AccountDto> _getAccountInfo;
+    private readonly IQueryHandler<GetAllUsers, IEnumerable<UserDto>> _getAllUsersHandler;
+    private readonly ICommandHandler<ChangeUserPassword> _changeUserPasswordHandler;
+    private readonly ITokenStorage _tokenStorage;
+
+    public UsersController(ICommandHandler<SignUp> signUpHandler, ICommandHandler<SignIn> signInHandler, ICommandHandler<DeleteUserAccount> deleteAccountHandler, ICommandHandler<ChangeUserRole> changeUserRoleHandler, IQueryHandler<GetAccountInfo, AccountDto> getAccountInfo, IQueryHandler<GetAllUsers, IEnumerable<UserDto>> getAllUsersHandler, ICommandHandler<ChangeUserPassword> changeUserPasswordHandler, ITokenStorage tokenStorage)
+    {
+        _signUpHandler = signUpHandler;
+        _signInHandler = signInHandler;
+        _deleteAccountHandler = deleteAccountHandler;
+        _changeUserRoleHandler = changeUserRoleHandler;
+        _getAccountInfo = getAccountInfo;
+        _getAllUsersHandler = getAllUsersHandler;
+        _changeUserPasswordHandler = changeUserPasswordHandler;
+        _tokenStorage = tokenStorage;
+    }
+    
+    [HttpPost("signUp")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> SignUp(SignUp command)
+    {
+        command = command with {UserId = Guid.NewGuid()};
+        await _signUpHandler.HandlerAsync(command);
+        
+        var user = await _getAccountInfo.HandlerAsync(new GetAccountInfo() {UserId = command.UserId});
+        return Ok(user);
+    }
+    
+    [HttpPost("signIn")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<JwtDto>> SignIn(SignIn command)
+    {
+        await _signInHandler.HandlerAsync(command);
+        var jwt = _tokenStorage.GetToken();
+        return Ok(jwt);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet("{userId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AccountDto>> GetUser(Guid userId)
+    {
+        var user = await _getAccountInfo.HandlerAsync(new GetAccountInfo() {UserId = userId});
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+    
+    [Authorize]
+    [HttpGet("myAccount")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AccountDto>> AccountInfo()
+    {
+        if (string.IsNullOrWhiteSpace(User.Identity?.Name))
+        {
+            return NotFound();
+        }
+        
+        var userId = Guid.Parse(User.Identity?.Name);
+        
+        var user = await _getAccountInfo.HandlerAsync(new GetAccountInfo() {UserId = userId});
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{userId:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteUserAccount(Guid userId)
+    {
+        await _deleteAccountHandler.HandlerAsync(new DeleteUserAccount(userId));
+        return NoContent();
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{userId:guid}/changeUserRole")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> ChangeUserRole(Guid userId, ChangeUserRole command)
+    {
+        await _changeUserRoleHandler.HandlerAsync(command with { UserId = userId, Role  = command.Role });
+        return Ok();
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers([FromQuery] GetAllUsers query)
+        => Ok(await _getAllUsersHandler.HandlerAsync(query));
+
+}
