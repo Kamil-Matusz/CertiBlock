152f6cbd59959cfe4aaac6d136b72fe230f57488
Awahir <matuszkamil076@gmail.com>
2025-07-29 20:00:34 +0200

    Add Users unit test & run migration script

---
+++ tests/Users/CertiBlock.Services.Users.UnitTests/Commands/SignInTests.cs
@@ -0,0 +1,22 @@
+﻿using CertiBlock.Services.Users.Application.Commands;
+using Shouldly;
+
+namespace CertiBlock.Services.Users.UnitTests.Commands;
+
+public class SignInTests
+{
+    [Fact]
+    public void signIn_shouldSetProperties_correctly()
+    {
+        // Arrange
+        var email = "testemail@test.com";
+        var password = "password";
+
+        // Act
+        var signInCommand = new SignIn(email, password);
+
+        // Assert
+        signInCommand.Email.ShouldBe(email);
+        signInCommand.Password.ShouldBe(password);
+    }
+}
