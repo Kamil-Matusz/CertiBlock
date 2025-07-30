7e13a7d7d4192d27b4c9be3664dbca37da18a88f
Awahir <matuszkamil076@gmail.com>
2025-07-30 17:57:38 +0200

    Strcture for the mongo connection

---
+++ src/Certificate/CertiBlock.Services.Certificates.Core/DAL/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CertiBlock.Services.Certificates.Core.DAL;
+
+public static class Extensions
+{
+    private const string SectionName = "mongo";
+    
+    public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
+    {
+        var options = new T();
+        var section = configuration.GetSection(sectionName);
+        section.Bind(options);
+
+        return options;
+    }
+}
