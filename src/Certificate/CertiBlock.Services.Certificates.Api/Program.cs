a9d790bd7e3ec048f83aa8434f940bcbfa4007bb
Awahir <matuszkamil076@gmail.com>
2025-07-30 17:48:03 +0200

    Add swagger to Certificate-Service

--- src/Certificate/CertiBlock.Services.Certificates.Api/Program.cs
+++ src/Certificate/CertiBlock.Services.Certificates.Api/Program.cs
@@ -5,16 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCore();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
