b516b37096d4f42c317869d3b59bc048f5b47c62
Awahir <matuszkamil076@gmail.com>
2025-08-13 22:37:38 +0200

    Init BlockchainRouter service

---
+++ src/BlockchainRouter/CertiBlock.Services.Blockchain.Core/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CertiBlock.Services.Blockchain.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        // Logger
+        services.AddLogging();
+        
+        return services;
+    }
+    
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseRouting();
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
+        
+        return app;
+    }
+}
