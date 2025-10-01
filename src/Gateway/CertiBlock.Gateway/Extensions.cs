ef0e73198cde1e1c5a49142a594731ab27f1393f
Awahir <matuszkamil076@gmail.com>
2025-10-01 22:06:36 +0200

    Add gateway module

---
+++ src/Gateway/CertiBlock.Gateway/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using CertiBlock.Shared.Logging;
+
+namespace CertiBlock.Gateway;
+
+public static class Extensions
+{
+    public static IServiceCollection AddGatewayExtensions(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Logger
+        services.AddLogging();
+        
+        // Seq
+        services.AddSeqLogging(configuration);
+        
+        return services;
+    }
+}
