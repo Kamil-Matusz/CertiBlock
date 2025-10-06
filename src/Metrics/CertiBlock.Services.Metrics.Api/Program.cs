5ba04226cfcdaa9bc5a5e3d273215b2b8df6592e
Awahir <matuszkamil076@gmail.com>
2025-10-06 21:34:03 +0200

    Create metrics service

---
+++ src/Metrics/CertiBlock.Services.Metrics.Api/Program.cs
@@ -0,0 +1,28 @@
+using CertiBlock.Services.Metrics.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services
+    .AddCore(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseInfrastructure();
+
+app.MapControllers();
+
+app.MapHealthChecks("/health");
+
+app.Run();
---
+++ src/Metrics/CertiBlock.Services.Metrics.Core/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using CertiBlock.Shared.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CertiBlock.Services.Metrics.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Logger
+        services.AddLogging();
+
+        // Seq
+        services.AddSeqLogging(configuration);
+
+        services.AddControllers();
+        
+        // HealthCheck
+        services.AddHealthChecks();
+        
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseRouting();
+
+        return app;
+    }
+
+    public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
+    {
+        var options = new T();
+        var section = configuration.GetRequiredSection(sectionName);
+        section.Bind(options);
+
+        return options;
+    }
+}
