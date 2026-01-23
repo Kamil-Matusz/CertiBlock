9cd4ad63feb22cb7649fa9310b19477e7c9928a8
Awahir <matuszkamil076@gmail.com>
2026-01-23 20:10:25 +0100

    Add index to polygon & ethereum

--- indexes/ethereum_indexes.js
+++
@@ -1,5 +0,0 @@
-use ethereum_db;
-
-db.ethereum.createIndex({ "CertificateId": 1 }, { "name": "Certificate_1_index" });
-
-db.ethereum_metrics.createIndex({ "CertificateId": 1 }, { "name": "Certificate_1_index" });
--- indexes/polygon_indexes.js
+++
@@ -1,5 +0,0 @@
-use polygon_db;
-
-db.polygon.createIndex({ "CertificateId": 1 }, { "name": "Certificate_1_index" });
-
-db.polygon_metrics.createIndex({ "CertificateId": 1 }, { "name": "Certificate_1_index" });
