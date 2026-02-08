c5e2be03ea270ac913cfb9a1caf03a1b98cef0c4
Awahir <matuszkamil076@gmail.com>
2026-02-08 15:30:18 +0100

    New views for the research metrics

--- src_frontend/src/admin/AdminApp.tsx
+++ src_frontend/src/admin/AdminApp.tsx
@@ -4,7 +4,9 @@ import { authProvider } from './authProvider';
 import { CertificateList } from './components/Certificates/CertificateList.tsx';
 import { CertificateShow } from './components/Certificates/CertificateShow.tsx';
 import { EthereumTransactionList } from './components/Ethereum/EthereumTransactionList';
+import { EthereumResearchMetrics } from './components/Ethereum/EthereumResearchMetrics';
 import { PolygonTransactionList } from './components/Polygon/PolygonTransactionList';
+import { PolygonResearchMetrics } from './components/Polygon/PolygonResearchMetrics';
 import { WalletBalanceChecker } from './components/Wallets/WalletBalanceChecker';
 import { CustomLoginPage } from './components/Auth/CustomLoginPage';
 import { WalletDashboard } from './components/Wallets/WalletDashboard';
@@ -13,6 +15,8 @@ import CardMembershipIcon from '@mui/icons-material/CardMembership';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
 
 export default function AdminApp() {
     return (
@@ -36,11 +40,24 @@ export default function AdminApp() {
                 options={{ label: 'Ethereum Transactions' }}
             />
             <Resource
+                name="ethereum-research-metrics"
+                list={EthereumResearchMetrics}
+                icon={BarChartIcon}
+                options={{ label: 'Ethereum Research Metrics' }}
+            />
+            <Resource
                 name="polygon-transactions"
                 list={PolygonTransactionList}
                 icon={AccountBalanceIcon}
                 options={{ label: 'Polygon Transactions' }}
             />
+            <Resource
+                name="polygon-research-metrics"
+                list={PolygonResearchMetrics}
+                icon={BarChartIcon}
+                options={{ label: 'Polygon Research Metrics' }}
+            />
+
             <Resource
                 name="wallet-checker"
                 list={WalletBalanceChecker}
