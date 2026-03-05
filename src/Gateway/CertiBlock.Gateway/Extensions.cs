103f6a1a48ca952ff32fe8136d3d4ac4e365a1ac
Awahir <matuszkamil076@gmail.com>
2026-03-05 18:23:42 +0100

    Change CORS policy

--- src/Gateway/CertiBlock.Gateway/Extensions.cs
+++ src/Gateway/CertiBlock.Gateway/Extensions.cs
@@ -9,7 +9,7 @@ public static class Extensions
     public static IServiceCollection AddGatewayExtensions(this IServiceCollection services, IConfiguration configuration)
     {
         // CORS
-        services.AddCorsPolicy();
+        services.AddCorsPolicy(configuration);
         
         // Logger
         services.AddLogging();
